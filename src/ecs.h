e8101c2083574aad78f3e6443170d9b2f90fbfbb
David Williams <david@aveday.net>
2016-06-03 18:09:14 +1000

    Add ecs.last_id, update comments

--- src/ecs.h
+++ src/ecs.h
@@ -15,25 +15,24 @@ template <typename T> std::vector<T> component_vector;
 template <typename T> uint64_t component_mask = NULL_MASK;
 
 struct ECS {
-    void run();
     int component_types = 1;
+    int last_id = ECS_ID;
     bool running = true;
     std::vector<uint64_t> entity_mask = std::vector<uint64_t>(MAX_ENTS);
     std::list<System*> systems;
 
+    /* Main API - only use in main program */
     ECS(std::list<System*> systems);
     void add_system(System* system);
+    void run();
 
+    /* System-facing API - must all be inline */
     inline bool check_mask(int entity, uint64_t mask);
     inline int new_entity();
     template <typename T> inline void add_component(int entity, T component);
     template <typename T> inline void remove_component(int entity);
-
-    /* Add a new component to global ECS context */
     template <typename T> inline void add_global_component(T component)
         { add_component(ECS_ID, component); }
-
-    /* Remove a component from the global ECS context */
     template <typename T> inline void remove_global_component()
         { remove_component<T>(ECS_ID); }
 };
@@ -45,6 +44,7 @@ int ECS::new_entity()
     while(entity_mask[e]) e++;
     if(e < MAX_ENTS) {
         entity_mask[e] = NULL_MASK;
+        last_id = e > last_id ? e : last_id;
         return e;
     } else {
         exit(EXIT_FAILURE);
@@ -55,7 +55,7 @@ int ECS::new_entity()
 template <typename T>
 void ECS::add_component(int entity, T component)
 {
-    // if first component type instance -> create maps
+    // create mask and vector on first instance of component type
     if(component_mask<T> == NULL_MASK) {
         component_mask<T> = 1 << component_types++;
         component_vector<T> = std::vector<T>(MAX_ENTS);
@@ -74,13 +74,14 @@ void ECS::remove_component(int entity)
         entity_mask[entity] &= ~component_mask<T>;
 }
 
-// check whether an entity fits a given mask
+// check entity has all mask components and null-mask bit isn't set
 bool ECS::check_mask(int entity, uint64_t mask)
 {
     return !(mask & (~entity_mask[entity] | NULL_MASK));
 }
 
-#ifdef ECS_IMPLEMENTATION
+#ifdef ECS_IMPLEMENTATION // MAIN API:
+
 /* Construct ECS with a list of systems */
 ECS::ECS(std::list<System*> systems)
 {
